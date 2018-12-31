936d77927ba022d357546ae9b44b8bb90853ab2d
mankaine <wkhaine97@gmail.com>
2018-12-30 21:33:17 -0800

    Removed posts from table

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_31_040030) do
+ActiveRecord::Schema.define(version: 2018_12_31_053108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "image_url"
-    t.text "notes"
-    t.datetime "coffee_made_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "reports", force: :cascade do |t|
     t.text "notes"
