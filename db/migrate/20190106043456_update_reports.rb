eb5892b6fe1cf5ec8217020231b9cd29d59c2467
mankaine <wkhaine97@gmail.com>
2019-01-05 22:28:55 -0800

    removed unecessary migrations

--- db/migrate/20190106043456_update_reports.rb
+++
@@ -1,6 +0,0 @@
-class UpdateReports < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :reports, :image_url
-    add_column :reports, :picture, :string
-  end
-end
--- db/migrate/20190106061757_add_pictures.rb
+++
@@ -1,5 +0,0 @@
-class AddPictures < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :reports, :image_url, :picture
-  end
-end
