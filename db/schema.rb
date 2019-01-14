12a880a1affd3a80259000634c8c2b8fca85966b
mankaine <wkhaine97@gmail.com>
2019-01-13 20:03:25 -0800

    Removed video title from view; most embedded videos provide title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_06_043456) do
+ActiveRecord::Schema.define(version: 2019_01_14_040143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2019_01_06_043456) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
   end
 
 end
